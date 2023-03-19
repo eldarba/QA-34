3cc9a23db8c4a992cad26bf1317e44e5f8277a6e
jbt <jbt@406-ins>
2023-03-19 14:25:17 +0200

    AAA

--- java-day1/src/b/DataTypes.java
+++ java-day1/src/b/DataTypes.java
@@ -1,6 +1,9 @@
 package b;
 
 public class DataTypes {
+	
+	// static - this code dosn't depend on a DataTypes object
+	// void - this method dosn't return any value when done
 
 	public static void main(String[] args) {
 		// create a variable - type + name + value
@@ -12,8 +15,31 @@ public class DataTypes {
 		
 		int x3 = 300;
 		long x4 = 400;
+		
 		System.out.println(x3);
 		System.out.println(x4);
+		
+		// non static method call requires an object of this class
+		// create an object
+		DataTypes object = new DataTypes();
+		// call the method
+		int theSum = object.sum(2, 4);
+		System.out.println(theSum);
+		
+		// static method call does not require an object of this class
+		theSum = sum2(3, 6);
+		System.out.println(theSum);
+		
+	}
+	
+	// non static method
+	public int sum(int a, int b) {
+		return a + b;
+	}
+	
+	// static method
+	public static int sum2(int a, int b) {
+		return a + b;
 	}
 	
 	

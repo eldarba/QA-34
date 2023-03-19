efdeee106bb0699c88d5d7a428d2f4d5b004f8c7
jbt <jbt@406-ins>
2023-03-19 14:11:01 +0200

    AAA

---
+++ java-day1/src/b/DataTypes.java
@@ -0,0 +1,20 @@
+package b;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// create a variable - type + name + value
+		byte x1 = 100;
+		System.out.println(x1);
+		
+		short x2 = 200;
+		System.out.println(x2);
+		
+		int x3 = 300;
+		long x4 = 400;
+		System.out.println(x3);
+		System.out.println(x4);
+	}
+	
+	
+}
